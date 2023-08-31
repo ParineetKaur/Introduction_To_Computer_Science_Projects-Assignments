9a2635ab7d0d4b0976096ff1e0efe5c0a458e11a
Parneet Kaur <117582430+ParineetKaur@users.noreply.github.com>
2023-08-31 10:25:15 -0700

    Update and rename Multiway.cpp to gradingSystem.cpp

---
+++ gradingSystem.cpp
@@ -0,0 +1,23 @@
+// Write multiway if-else statements for which the output is “You got A!” if the value of the variable finalScore is greater than equal to 90, the output is “You got a B!” if the value of finalScore is greater than equal to 80, the output is “You got a C!” if the value of finalScore is greater than equal to 70, the output is “You got a D!” if the value of finalScore is greater than equal to 60, otherwise the output is “You got F!”
+
+if(finalScore>=90){
+  cout<<"You got A!";
+}
+
+else if(finalScore>=80){
+  cout<<"You got B!";
+}
+
+else if(finalScore>=70){
+  cout<<"You got C!";
+}
+
+else if(finalScore>=60){
+  cout<<"You got D!";
+}
+
+else{
+  cout<<"You got F!"
+}
+
+//Always read such questions very carefully
