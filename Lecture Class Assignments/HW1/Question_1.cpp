947604a314d684705ddff5320e17d01b3256fe16
Parneet Kaur <117582430+ParineetKaur@users.noreply.github.com>
2023-08-31 15:50:34 -0700

    Delete Lecture Class Assignments/HW1 directory

--- Lecture Class Assignments/HW1/Question_1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-double gallons;
-cout << "Provide a numerical value for gallons (so that it can be converted to liters): ";
-cin >> gallons;
-cout << gallons * 3.785;
-}
