9a957df44f0bfd343e078e7b6a6098ff7a889b97
Parneet Kaur <117582430+ParineetKaur@users.noreply.github.com>
2023-10-13 09:36:11 -0700

    Add files via upload

---
+++ Lab Projects/Prob2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void findMin(int a[], int min, int size);
+
+int main() {
+
+int a[5];
+int min;
+
+findMin(a, min, 5);
+return 0;
+}
+
+void findMin(int a[], int min, int size) {
+cout<<"Enter "<<size<<" numbers:"<<endl;
+for (int i = 0; i<size; i++) {
+    cin>>a[i];
+}
+min = a[0];
+for (int i = 0; i < size; i++){
+   if(a[i] < min) {
+            	min = a[i];
+            }
+}
+cout<<"The minimum element in this array is: "<<min<<endl;
+}
---
+++ Lab Projects/Prob6.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+void factors(int n, int facts[]);
+
+int main() {
+
+int facts[50];
+int n = 0;
+
+cout<<"Enter a positive number: "<<endl;
+cin>>n;
+if(n>=50) {
+  cout<<"Sorry,can't test numbers that high!"<<endl;
+}
+else {
+  factors(n, facts);
+}
+return 0;
+}
+
+void factors(int n, int facts[]) {
+  bool a;
+  for(int i=1; i<=n; i++) {
+    std::cout<<"n mod i "<<n%i << '\n';
+    if(n%i==0) {
+      a=true;
+      cout<<i<<":"<<a<<endl;
+    }
+    else {
+      a=false;
+      cout<<i<<":"<<a<<endl;
+    }
+  }
+}
